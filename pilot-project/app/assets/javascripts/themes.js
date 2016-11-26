434c9256edf2a9021ed1aa5434a13a76f37ef3cd
Ho Viet Tuan <tuanhv258@gmail.com>
2016-11-27 00:31:42 +0700

    login logout add themes

---
+++ pilot-project/app/assets/javascripts/themes.js
@@ -0,0 +1,9 @@
+//=require themes/jquery.js 
+//=require themes/bootstrap.min.js 
+//=require themes/jquery.scrollUp.min.js 
+//=require themes/price-range.js 
+//=require themes/jquery.prettyPhoto.js 
+//=require themes/main.js 
+//=require themes/contact.js 
+//=require themes/gmaps.js 
+//=require themes/html5shiv.js 
---
+++ pilot-project/app/controllers/indexes_controller.rb
@@ -0,0 +1,5 @@
+class IndexesController < ApplicationController
+  def index 
+
+  end
+end  
